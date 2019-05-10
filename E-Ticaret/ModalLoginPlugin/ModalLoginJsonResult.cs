63d6dc025732c9b7255aae61d829a8eaafaae646
gulay.senturk <gulay.senturk@402----11>
2019-05-10 09:15:00 +0300

    Add project files.

---
+++ E-Ticaret/ModalLoginPlugin/ModalLoginJsonResult.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_Ticaret.Infrastructure.Abstract;
+
+namespace E_Ticaret.ModalLogin
+{
+    internal partial class ModalLoginJsonResult : MyJsonResult<string>
+    {
+
+    }
+}
